ff5bab82e548f1d7f3cfb17d58e185154d26aff1
Patel <PSP34@pitt.edu>
2024-01-24 15:54:21 -0500

    String Slicing and Formating

--- strings.py
+++ strings.py
@@ -56,9 +56,9 @@ print(first_name + ' ' + last_name + ':' + str(age))
 print("hello"*3)
 
 # accessing string characters - a string is just a sequence of character
-
 '''
 
+'''
 name  = "Jane Doe"
 print(name[2])
 # print(name[8])  # throws index out of bound error
@@ -66,5 +66,38 @@ print(name[2])
 # Retrieving the character at a given index
 print(name.index('o'))  # returns 6
 print(name.index('e'))  # returns the index of first occurence
+'''
+
+#### String Slicing #####
+
+emp_name = "Jane Doe"
+print(emp_name[2:6])
+print(emp_name[0:4])
+print(emp_name[:4])
+print(emp_name[3:])
+print(emp_name[-4:-1])
+print(emp_name[1:6:2])  # its telling it to stay in between 1 and 6 and skip 2 characters
+print(emp_name.count('e'))  # its counting how many e are in the whole name
+print(emp_name.find('doe'))  # position of te sub string
+print(emp_name.replace('Jane','John'))
+print(emp_name)
+emp_name = emp_name.replace('Jane','John')
+print(emp_name)
+
+print('oh' in emp_name) # we checked if oh is the part of the string.
+
+## STRING FORMATING ##
+
+Student_name = "Alex"
+Score = 74
+
+print("Name: " + Student_name + " " + str(Score))
+print("Name: {} Score: {} ".format(Student_name,Score))
+
+# f-Strings
+
+print(f"Name: {Student_name} Score: {Score}")
+print(f"3 times 10 is equal to {3*10}")
+
 
 
